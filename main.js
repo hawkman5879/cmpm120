65423d2bc70656fa7982c7492899d3aee039444a
hawkman5879 <bamarias@ucsc.edu>
2018-05-06 15:10:03 -0700

    Changed the loop

---
+++ main.js
@@ -0,0 +1,19 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
+var i = 2;
+// define MainMenu state and methods
+var MainMenu = function(game) {};
+MainMenu.prototype = {
+	preload: function() {
+		
+	},
+	create: function() {
+	},
+	update: function() {
+		// main menu logic
+		if (i = 2)
+			console.log("we Op");
+		}
+	}
+}
+
+game.state.start('MainMenu');
